daa948c712e9dddb5a78b66c32f96c6afe8e06d4
Tim Green <greent@vmware.com>
2014-03-05 08:12:04 -0500

    create repo if iso exists

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -19,13 +19,15 @@ class razor::instance (
   }
 
   # EXAMPLE: razor -d create-repo --name=esx-5.5 --iso-url file:///tmp/VMware-VMvisor-Installer-5.5.0-1331820.x86_64.iso
-  #exec { 'razor -d create-repo':
-  #  command => "/usr/local/bin/razor -d create-repo --name ${target_os} --iso-url ${iso}"
-  #}
+  exec { 'razor -d create-repo':
+    command => "/usr/local/bin/razor -d create-repo --name ${target_os} --iso-url ${iso}",
+    onlyif => "test -f ${iso}",
+  }
 
-  #exec { 'razor create-policy':
-  #  command => "/usr/local/bin/razor create-policy --json /tmp/policy${::hostname}.json",
-  #  require => [ File["/tmp/policy${::hostname}.json"], Exec['razor create-policy'] ],
-  #}
+  exec { 'razor create-policy':
+    command   => "/usr/local/bin/razor create-policy --json /tmp/policy${::hostname}.json",
+    require   => [ File["/tmp/policy${::hostname}.json"], Exec['razor create-policy'] ],
+    subscribe => Exec['razor -d create-repo'],
+  }
 
 }
